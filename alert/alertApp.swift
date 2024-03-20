d4211c8311cf5899dbe759e8ea1efe619dff2eb2
kota-aso <kotaaso1212@gmail.com>
2024-03-20 17:33:20 +0900

    Initial Commit

---
+++ alert/alertApp.swift
@@ -0,0 +1,17 @@
+//
+//  alertApp.swift
+//  alert
+//
+//  Created by Kota Aso on 2024/03/20.
+//
+
+import SwiftUI
+
+@main
+struct alertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
